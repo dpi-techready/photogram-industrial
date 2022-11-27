1345b4ac2801394f7649a28cc8ebaab192a08df7
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-11-27 18:48:05 +0000

    Added commenting and fixed user route

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,8 @@
 require "active_support/core_ext/integer/time"
 
-# [SOLVED]-IMAGE IN ASSET PIPELINE NOT FOUND-RUBY
-config.serve_static_assets = true
-config.assets.compile = true
-
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  resources :users, only: :show
+  # resources :users, only: :show
+
+  get "/:username" => "users#show", as: :user
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
